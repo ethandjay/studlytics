433cd3c79e3e549aa346816fc6734589ac62b3ab
Ethan Jaynes <ethandjay@gmail.com>
2017-11-07 19:57:12 -0600

    only keeps most recent revision

--- studlife_insight.py
+++ studlife_insight.py
@@ -3,8 +3,13 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk import tokenize
 from bs4 import BeautifulSoup
 from multiprocessing import Pool
+import warnings
 
 def cleanContent(line):
+
+	# Filter bs4 URL warnings
+	warnings.filterwarnings("ignore", category=UserWarning, module='bs4')
+
 	soup = BeautifulSoup(line[4], 'html.parser')
 
 	# Strip tags
@@ -30,36 +35,46 @@ def initPool():
     global sentiment
     sentiment = SentimentIntensityAnalyzer()
 
-with open('../Downloads/testfile.csv') as corpus:
+with open('./wp_1_posts.csv') as corpus:
 	file = csv.reader(corpus)
 	scores = collections.OrderedDict()
-	pool = Pool()
-	cleaned = pool.map(cleanContent, file)
+
+	print("Cleaning content...")
+	# Clean post content
+	cleaned = map(cleanContent, file)
 
 	# Remove attachments, post duplicates (keep only last revision)
-	keep = []
+
+	## Create dict of post_id : list of reivision_id's, in order
+
+	print("Removing duplicates and non-text posts...")
+	keep = {}
 	for record in cleaned:
 		if len(record[4]) < 300 or record[21] == 'attachment':
 			continue
 		else:
 			if record[18] == '0':
-				keep.append(record[0])
+				keep[int(record[0])] = [record[0]]
 			else:
-				if record[18] in keep: keep.remove(record[18])
-				keep.append(record[0])
+				if int(record[18]) in keep.keys():
+					keep[int(record[18])].append(int(record[0]))
+				else:
+					keep[int(record[18])] = [record[18], record[0]]
 
-	cleaned = {x: cleaned[x] for x in keep}
+	# Save last id of revision for each post
+	keep = [value[-1] for key, value in keep.items()]
 
-	for p in keep:
-		print(p)
+	# Filter csv data by these reivision id's
+	cleaned = [x for x in cleaned if int(x[0]) in keep]
 
-# 	pool = Pool(8, initPool, ())
-# 	scores = pool.map(analyzeSentiment, cleaned_posts)
+	print("Analyzing sentiment...")
+	pool = Pool(8, initPool, ())
+	scores = pool.map(analyzeSentiment, cleaned)
 
-# sorted_scored = sorted(scores, key=lambda x:x[1]['neg'])
+sorted_scored = sorted(scores, key=lambda x:x[1]['neg'])
 
-# for item in sorted_scored:
-# 	print(item[0])
-# 	print(item[1])
-# 	print 
+for item in sorted_scored:
+	print(item[0])
+	print(item[1])
+	print 
 
